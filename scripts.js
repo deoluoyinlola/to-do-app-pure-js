331b3433c4b9cf51ff159a3d87190eb093234389
Adeolu Oyinlola <adeoluoyin@gmail.com>
2022-01-26 15:45:56 -0800

    edit functionality

--- scripts.js
+++ scripts.js
@@ -32,13 +32,56 @@ function createTodo() {
     addInput.value = " ";
 }
 
+function showEditInput (paragraphElement) {
+
+    var editInput = document.getElementsByName("editInput")[0];
+    if (editInput) {
+        editInput.remove();
+    }
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = "editInput";
+    input.value = paragraphElement.textContent;
+    input.classList.add("editInput");
+
+    paragraphElement.parentElement.appendChild(input);
+    input.focus();
+}
+
+function updateTodo() {
+
+    var editInput = document.getElementsByName("editInput")[0];
+    if (!editInput) {
+        return;
+    }
+
+    var newText = editInput.value;
+
+    if (newText !== "") {
+        var paragraph = editInput.parentElement.querySelector(".paragraph");
+        paragraph.textContent = newText;
+    }
+
+    editInput.remove();
+}
+
 function removeTodo(removeElement) {
     removeElement.parentElement.remove();
 }
 
-
+function toggleComplete(inputElement) {
+    if (inputElement.checked === false) {
+        inputElement.parentElement.classList.remove("complete");
+    } else {
+        inputElement.parentElement.classList.add("complete");
+    }
+}
 
 list.addEventListener("click", function (event) {
+
+    event.stopPropagation();
+
     switch (event.target.tagName) {
         case "P":
             showEditInput();
@@ -55,7 +98,18 @@ list.addEventListener("change", function (event) {
     }
 });
 
-addBtn.addEventListener("click", createTodo);
+list.addEventListener("keypress", function(event) {
+    if (event.target.tagName === "INPUT" && event.target.type === "text" && event.key === "Enter") {
+        updateTodo();
+    }
+});
+
+document.addEventListener("click", updateTodo);
+
+addBtn.addEventListener("click", function (event) {
+    event.stopPropagation();
+    createTodo();
+});
 addInput.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         createTodo();
